8f311c4d642c8ab56879aad468e82dd275842dfa
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-07-21 20:06:44 +0900

    Update W10_Advanced2_005.h

--- src/W10_test/W10_Advanced2_005.h
+++ src/W10_test/W10_Advanced2_005.h
@@ -263,7 +263,7 @@ void W10_loadJsonConfig(){
                     Serial.println("\nparsed json");
                     // 설정 구조체에 값 로드
                     strcpy(g_W10_appConfig.mqttServer   , v_jsonDoc["mqtt_server"]  | "");
-					g_W10_appConfig.mqttPort            = v_jsonDoc["mqtt_port"]    | 1883);  
+					g_W10_appConfig.mqttPort            = v_jsonDoc["mqtt_port"]    | 1883;  
                     // strcpy(g_W10_appConfig.mqttPort     , v_jsonDoc["mqtt_port"]    | "8080");
                     strcpy(g_W10_appConfig.apiToken     , v_jsonDoc["api_token"]    | "YOUR_APITOKEN");
 
